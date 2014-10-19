3b6aab69fac3b06f79e10f0c0b89b4ab96070290
ArtyTheTorch <artysaligator@yahoo.com>
2014-10-19 11:42:53 -0700

    Returns the Second highest value in a int array
    UI-Interface

---
+++ 9SecondHighestNumberIntArray/FindSecondHighestInt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _9SecondHighestNumberIntArray
+{
+    class FindSecondHighestInt
+    {
+        int[] intArray;
+
+        public FindSecondHighestInt(string[] p)
+        {
+            intArray = new int[p.Length];
+            for (int i = 0; i < p.Length; i++) {
+                intArray[i] = Convert.ToInt32(p[i]);
+            }
+        }
+
+        public string findIt()
+        {
+            int highest=0, secondHighest=0;
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (intArray[i] > highest)
+                {
+                    highest = intArray[i];
+                }
+            }
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (intArray[i] > secondHighest&& intArray[i] != highest)
+                {
+                    secondHighest = intArray[i];
+                }
+            }
+            return secondHighest.ToString();
+        }
+    }
+}
---
+++ 9SecondHighestNumberIntArray/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _9SecondHighestNumberIntArray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result;
+            FindSecondHighestInt x = new FindSecondHighestInt(textBox1.Text.Split(','));
+            result = x.findIt();
+            label2.Text = result;
+        }
+    }
+}
